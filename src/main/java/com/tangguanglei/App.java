8629f2fd2c5e833f0de8c0b2eea91080a686f573
tangguanglei <tangguanglei@oneship.cc>
2019-09-25 10:34:41 +0800

    init git

---
+++ src/main/java/com/tangguanglei/App.java
@@ -0,0 +1,13 @@
+package com.tangguanglei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
